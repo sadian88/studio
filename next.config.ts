2dd9bd85ed59c09148204bd9343c81913709399b
Antonio Diaz S. <sadian88@gmail.com>
2025-06-05 23:09:28 +0000

    quiero generar el build pero en una carpeta llamada build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'build', // <<< Added this line
   typescript: {
     ignoreBuildErrors: true,
   },
